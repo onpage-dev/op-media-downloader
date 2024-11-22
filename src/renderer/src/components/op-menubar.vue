a7f6ddb4c48737a10bddce2da2443bc26b3d02dd
Andrian Leah <andrian.leah@gmail.com>
2024-11-22 11:23:42 +0100

    #feat update header, info modal and op-modal

--- src/renderer/src/components/op-menubar.vue
+++ src/renderer/src/components/op-menubar.vue
@@ -4,13 +4,10 @@ import { computed, ref } from 'vue'
 import { useI18n } from 'vue-i18n'
 import OpLogo from './op-logo.vue'
 import OpModal from './op-modal.vue'
+const props = defineProps<{
+  storage: StorageService
+}>()
 const i18n = useI18n()
-const props = defineProps({
-  storage: {
-    type: StorageService,
-    required: true,
-  },
-})
 const show_info = ref(false)
 const dark_mode = computed(() => props.storage.user_properties.dark_mode)
 const italian = computed(() => i18n.locale.value == 'it')
@@ -28,158 +25,168 @@ const is_outdated = computed(() => {
   )
 })
 </script>
-
 <template>
-  <OpModal v-if="show_info" @close="show_info = false">
-    <div class="full-height-scroll gap-unit">
-      <!-- Title -->
-      <h2 class="flex flex-row gap-unit items-start">
-        <div class="flex flex-row items-start">
-          On Page
-          <div class="text-sm mt-unit">®</div>
-        </div>
-        Media Downloader
-      </h2>
+  <Transition name="modal-fade">
+    <OpModal v-if="show_info" @close="show_info = false">
+      <div class="full-height-scroll gap-unit">
+        <!-- Title -->
+        <h2 class="flex flex-row gap-unit items-start">
+          <div class="flex flex-row items-start">
+            On Page
+            <div class="text-sm mt-unit">®</div>
+          </div>
+          Media Downloader
+        </h2>
 
-      <div class="full-height-scroll gap-unit-double">
-        <template v-if="italian">
-          <!-- Italian Subtitle -->
-          <p>
-            Questo programma permette di scaricare tutti i file da un dato
-            progetto tramite un
-            <span
-              class="link"
-              @click="
-                openURL('https://app.onpage.it/#/help/integrations/token-api')
-              "
-            >
-              Token Api</span
-            >.
-          </p>
-
-          <!-- Italian Use manual -->
-          <div class="flex flex-col gap-unit">
-            <h3>Come funziona</h3>
-
-            <p>Per iniziare, aggiungi un nuovo progetto specificando:</p>
-            <ol class="pl-unit">
-              <li class="flex-row-center-unit">
-                <op-icon icon="circle" style="font-size: 0.4rem" /> Nome locale
-                per identificare il progetto
-              </li>
-              <li class="flex-row-center-unit">
-                <op-icon icon="circle" style="font-size: 0.4rem" /> Token Api
-                del progetto che verrà utilizzato per scaricare i file
-              </li>
-              <li class="flex-row-center-unit">
-                <op-icon icon="circle" style="font-size: 0.4rem" /> Cartella
-                dove salvare i file scaricati
-              </li>
-              <li class="flex-row-center-unit">
-                <op-icon icon="circle" style="font-size: 0.4rem" /> Se mantenere
-                o meno i vecchi file scaricati non più presenti su On Page
-              </li>
-            </ol>
-
-            <p class="pt-unit">
-              Per ogni progetto potrai effettuare le seguenti azioni:
+        <div class="full-height-scroll gap-unit-double">
+          <template v-if="italian">
+            <!-- Italian Subtitle -->
+            <p>
+              Questo programma permette di scaricare tutti i file da un dato
+              progetto tramite un
+              <span
+                class="link"
+                @click="
+                  openURL(
+                    'https://app.onpage.it/#/help/integrations/token-api-k',
+                  )
+                "
+              >
+                Token Api</span
+              >.
             </p>
-            <ol class="pl-unit">
-              <li class="flex-row-center-unit">
-                <op-icon icon="circle" style="font-size: 0.4rem" />
-                Sincronizzare i file locali con quelli presenti sul progetto
-                remoto
-              </li>
-              <li class="flex-row-center-unit">
-                <op-icon icon="circle" style="font-size: 0.4rem" /> Modificare
-                le proprietà della configurazione del progetto locale definite
-                in fase di creazione
-              </li>
-              <li class="flex-row-center-unit">
-                <op-icon icon="circle" style="font-size: 0.4rem" /> Eliminare il
-                progetto locale scegliendo se mantenere o meno i file scaricati
-              </li>
-            </ol>
-          </div>
-        </template>
-        <template v-else>
-          <!-- English Subtitle -->
-          <p>
-            This program allows to download all files from a given project using
-            an
-            <span
-              class="link"
-              @click="
-                openURL('https://app.onpage.it/#/help/integrations/token-api')
-              "
-            >
-              API Token</span
-            >.
-          </p>
 
-          <!-- Italian Use manual -->
-          <div class="flex flex-col gap-unit">
-            <h3>How it works</h3>
+            <!-- Italian Use manual -->
+            <div class="flex flex-col gap-unit">
+              <h3>Come funziona</h3>
 
-            <p>To start, add a new project setting:</p>
-            <ol class="pl-unit">
-              <li class="flex-row-center-unit">
-                <op-icon icon="circle" style="font-size: 0.4rem" /> Name, used
-                as a local identifier of the project
-              </li>
-              <li class="flex-row-center-unit">
-                <op-icon icon="circle" style="font-size: 0.4rem" /> Api Token of
-                the remote project that'll be used to download the files
-              </li>
-              <li class="flex-row-center-unit">
-                <op-icon icon="circle" style="font-size: 0.4rem" /> Destination
-                Folder where the software will save the downloaded files
-              </li>
-              <li class="flex-row-center-unit">
-                <op-icon icon="circle" style="font-size: 0.4rem" /> If you'd
-                like to keep the local files that are not present on On Page
-                anymore
-              </li>
-            </ol>
+              <p>Per iniziare, aggiungi un nuovo progetto specificando:</p>
+              <ol class="pl-unit">
+                <li class="flex-row-center-unit">
+                  <op-icon icon="circle" style="font-size: 0.4rem" /> Nome
+                  locale per identificare il progetto
+                </li>
+                <li class="flex-row-center-unit">
+                  <op-icon icon="circle" style="font-size: 0.4rem" /> Token Api
+                  del progetto che verrà utilizzato per scaricare i file
+                </li>
+                <li class="flex-row-center-unit">
+                  <op-icon icon="circle" style="font-size: 0.4rem" /> Cartella
+                  dove salvare i file scaricati
+                </li>
+                <li class="flex-row-center-unit">
+                  <op-icon icon="circle" style="font-size: 0.4rem" /> Se
+                  mantenere o meno i vecchi file scaricati non più presenti su
+                  On Page
+                </li>
+              </ol>
 
-            <p class="pt-unit">For each project you'll be able to:</p>
-            <ol class="pl-unit">
-              <li class="flex-row-center-unit">
-                <op-icon icon="circle" style="font-size: 0.4rem" />
-                Sync the local files with the remote ones
-              </li>
-              <li class="flex-row-center-unit">
-                <op-icon icon="circle" style="font-size: 0.4rem" /> Edit the
-                project settings defined during creation
-              </li>
-              <li class="flex-row-center-unit">
-                <op-icon icon="circle" style="font-size: 0.4rem" /> Delete the
-                local project and, eventually, the downloaded files too
-              </li>
-            </ol>
-          </div>
-        </template>
+              <p class="pt-unit">
+                Per ogni progetto potrai effettuare le seguenti azioni:
+              </p>
+              <ol class="pl-unit">
+                <li class="flex-row-center-unit">
+                  <op-icon icon="circle" style="font-size: 0.4rem" />
+                  Sincronizzare i file locali con quelli presenti sul progetto
+                  remoto
+                </li>
+                <li class="flex-row-center-unit">
+                  <op-icon icon="circle" style="font-size: 0.4rem" /> Modificare
+                  le proprietà della configurazione del progetto locale definite
+                  in fase di creazione
+                </li>
+                <li class="flex-row-center-unit">
+                  <op-icon icon="circle" style="font-size: 0.4rem" /> Eliminare
+                  il progetto locale scegliendo se mantenere o meno i file
+                  scaricati
+                </li>
+              </ol>
+            </div>
+          </template>
+          <template v-else>
+            <!-- English Subtitle -->
+            <p>
+              This program allows to download all files from a given project
+              using an
+              <span
+                class="link"
+                @click="
+                  openURL(
+                    'https://app.onpage.it/#/help/integrations/token-api-k',
+                  )
+                "
+              >
+                API Token</span
+              >.
+            </p>
+
+            <!-- Italian Use manual -->
+            <div class="flex flex-col gap-unit">
+              <h3>How it works</h3>
+
+              <p>To start, add a new project setting:</p>
+              <ol class="pl-unit">
+                <li class="flex-row-center-unit">
+                  <op-icon icon="circle" style="font-size: 0.4rem" /> Name, used
+                  as a local identifier of the project
+                </li>
+                <li class="flex-row-center-unit">
+                  <op-icon icon="circle" style="font-size: 0.4rem" /> Api Token
+                  of the remote project that'll be used to download the files
+                </li>
+                <li class="flex-row-center-unit">
+                  <op-icon icon="circle" style="font-size: 0.4rem" />
+                  Destination Folder where the software will save the downloaded
+                  files
+                </li>
+                <li class="flex-row-center-unit">
+                  <op-icon icon="circle" style="font-size: 0.4rem" /> If you'd
+                  like to keep the local files that are not present on On Page
+                  anymore
+                </li>
+              </ol>
+
+              <p class="pt-unit">For each project you'll be able to:</p>
+              <ol class="pl-unit">
+                <li class="flex-row-center-unit">
+                  <op-icon icon="circle" style="font-size: 0.4rem" />
+                  Sync the local files with the remote ones
+                </li>
+                <li class="flex-row-center-unit">
+                  <op-icon icon="circle" style="font-size: 0.4rem" /> Edit the
+                  project settings defined during creation
+                </li>
+                <li class="flex-row-center-unit">
+                  <op-icon icon="circle" style="font-size: 0.4rem" /> Delete the
+                  local project and, eventually, the downloaded files too
+                </li>
+              </ol>
+            </div>
+          </template>
+        </div>
+
+        <div class="flex flex-row gap-unit">
+          <op-clickable-tag
+            @click="
+              openURL('https://github.com/onpage-dev/op-media-downloader')
+            "
+          >
+            <op-icon icon="github" type="brand" /> Git
+          </op-clickable-tag>
+          <op-clickable-tag
+            pad="px-unit-double py-0"
+            @click="openURL('https://app.onpage.it/#/help/On-page-media-k')"
+          >
+            <OpLogo
+              :dark="dark_mode"
+              class="w-auto object-contain my-unit flex-shrink-0"
+              style="height: 0.7rem"
+            />
+          </op-clickable-tag>
+        </div>
       </div>
-
-      <div class="flex flex-row gap-unit">
-        <op-clickable-tag
-          @click="openURL('https://github.com/onpage-dev/op-media-downloader')"
-        >
-          <op-icon icon="github" type="brand" /> Git
-        </op-clickable-tag>
-        <op-clickable-tag
-          pad="px-unit-double py-0"
-          @click="openURL('https://app.onpage.it/#/help')"
-        >
-          <OpLogo
-            :dark="dark_mode"
-            class="w-auto object-contain my-unit flex-shrink-0"
-            style="height: 0.7rem"
-          />
-        </op-clickable-tag>
-      </div>
-    </div>
-  </OpModal>
+    </OpModal>
+  </Transition>
   <div class="flex flex-col text-center py-10">
     <div
       class="flex flex-row items-end cursor-pointer group"
@@ -196,27 +203,28 @@ const is_outdated = computed(() => {
         style="margin-left: -1rem"
       />
     </div>
-    <div>
+    <div class="flex-row-center mx-auto gap-unit-half">
       Media Downloader
       <span
-        v-if="version_info"
-        class="rounded-sm text-dark"
-        :class="is_outdated ? 'bg-red' : 'bg-brand'"
+        v-if="version_info?.current"
+        class="border-b-2"
+        :class="is_outdated ? 'border-red' : 'border-brand'"
         style="padding: 2px"
       >
-        {{ version_info?.current ?? '?' }}
+        {{ version_info.current }}
       </span>
+      <a
+        v-if="is_outdated && version_info?.latest"
+        class="flex-row-center gap-unit-half"
+        :href="`https://github.com/onpage-dev/op-media-downloader/releases/tag/${version_info.latest}`"
+        target="_blank"
+      >
+        <op-icon icon="arrow-right" />
+        <span class="border-b-2 border-blue" style="padding: 2px">
+          {{ $t('download') }}
+          {{ version_info.latest }}
+        </span>
+      </a>
     </div>
-    <a
-      v-if="is_outdated && version_info?.latest"
-      class="mt-2"
-      :href="`https://github.com/onpage-dev/op-media-downloader/releases/tag/${version_info.latest}`"
-      target="_blank"
-    >
-      <span class="bg-blue rounded-sm text-dark" style="padding: 2px">
-        {{ $t('download') }}
-        {{ version_info.latest }}
-      </span>
-    </a>
   </div>
 </template>
--- src/renderer/src/components/op-modal.vue
+++ src/renderer/src/components/op-modal.vue
@@ -1,25 +1,39 @@
 <script lang="ts" setup>
-const emit = defineEmits(['close'])
+const emit = defineEmits<{
+  close: []
+}>()
 </script>
 <template>
-  <div
-    class="animate-fadein-slow overlay z-50 bg-overlay flex-col-center justify-center"
-    @click="emit('close')"
-  >
-    <div
-      class="full-height-scroll justify-center p-unit-double gap-unit op-modal"
-    >
-      <div class="flex-row-center-unit justify-end">
-        <op-circle-btn class="sober-link-danger" @click.stop="emit('close')">
-          <op-icon icon="x" />
+  <div class="overlay bg-overlay z-50 flex-col" @click="emit('close')">
+    <div class="op-modal full-height-scroll h-full p-unit-double">
+      <op-overlay-card class="op-modal-content m-auto" col @click.stop>
+        <op-circle-btn
+          class="top-0 right-0 group"
+          position="absolute"
+          radius="md"
+          size="9"
+          @click.stop="emit('close')"
+        >
+          <op-icon icon="x" class="group-hover:text-red duration-100" />
         </op-circle-btn>
-      </div>
-      <op-overlay-card
-        class="flex flex-col relative w-full overflow-auto overflow-x-hidden op-modal-content"
-        @click.stop
-      >
+
         <slot />
       </op-overlay-card>
     </div>
   </div>
 </template>
+<style lang="scss">
+/** Modal Fade */
+.modal-fade-enter-from,
+.modal-fade-leave-to {
+  @apply opacity-0;
+}
+.modal-fade-enter-active,
+.modal-fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+.modal-fade-enter-to,
+.modal-fade-leave-from {
+  @apply opacity-100;
+}
+</style>
